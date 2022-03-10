ee139d42dd1c00ea337974b70b1455dc893226b3
chill7627 <sethhill7627@gmail.com>
2022-03-09 21:18:31 -0500

    add minting function and connected to kryptobirds

---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/*
+    building out a mint function
+       a. nft to point to an address
+       b. keep track of the token ids
+       c. keep track of token owner addresses to token ids
+       d. keep track of how many tokens an owner address has
+       e. emit and keep track of logged info
+        create an event that emits a transfer log -contract addres, 
+        where it is being minted to, the id
+*/
+
+contract ERC721 {
+
+    // events for logging purposes
+    event Transfer(address indexed from,
+                   address indexed to, 
+                   uint256 indexed tokenId);
+
+    // mapping from token id to owner
+    mapping(uint256 => address) private _tokenOwner;
+    // mapping from owner to number of owned tokens
+    mapping(address => uint256) private _ownedTokens;
+
+    function _exists(uint256 tokenId) internal view returns(bool) {
+        // try to access owner of token and if exists returns true, not false
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
+
+    function _mint(address to, uint256 tokenId) internal {
+        // require the address is not blank
+        require(to != address(0), "ERC721: minting to zero address");
+        // require that token hasn't been minted already
+        require(!_exists(tokenId), "ERC721: token already minted");
+        // add address to owner of token id
+        _tokenOwner[tokenId] = to;
+        // add 1 to owned token counts for address
+        _ownedTokens[to] += 1;
+
+        // emit transfer event
+        emit Transfer(address(0), to, tokenId);
+    }
+    
+}
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -4,6 +4,24 @@ pragma solidity ^0.8.0;
 import './ERC721Connector.sol';
 
 contract Kryptobird is ERC721Connector {
+    // create array to keep track of kbirdz nfts in contract
+    string[] public kryptoBirdz;
+
+    // keep track of minted birds
+    mapping(string => bool) _kryptoBirdzExists;
+
+    function mint(string memory _kryptoBird) public {
+        require(!_kryptoBirdzExists[_kryptoBird],
+        "Error kryptoBird already exists");
+        // this deprecated uint256 _id = KrypotBirdz.push(_krypotBird);
+        // add minted bird to string array and use its position in the array as the tokenId
+        kryptoBirdz.push(_kryptoBird);
+        uint _id = kryptoBirdz.length - 1;
+        _mint(msg.sender, _id);
+
+        // mark bird as exists
+        _kryptoBirdzExists[_kryptoBird] = true;
+    }
 
     constructor() ERC721Connector('KryptoBird','KBIRDZ') {
 
