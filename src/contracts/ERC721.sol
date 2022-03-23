8884fc3a1c6a1e7de5002e186267a6cab148a6a6
chill7627 <sethhill7627@gmail.com>
2022-03-22 22:04:37 -0400

    added interface for erc721

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import './ERC165.sol';
+import './interfaces/IERC721.sol';
 
 /*
     building out a mint function
@@ -14,16 +15,7 @@ import './ERC165.sol';
         where it is being minted to, the id
 */
 
-contract ERC721 is ERC165 {
-
-    // events for logging purposes
-    event Transfer(address indexed from,
-                   address indexed to, 
-                   uint256 indexed tokenId);
-    
-    event Approval(address indexed owner,
-                   address indexed approved,
-                   uint256 tokenId);
+contract ERC721 is ERC165, IERC721 {
 
     // mapping from token id to owner
     mapping(uint256 => address) private _tokenOwner;
@@ -33,13 +25,13 @@ contract ERC721 is ERC165 {
     mapping(uint256 => address) private _tokenApprovals;
 
     // balance of function to get balance return number of tokens owned by an owner
-    function balanceOf(address _owner) public view returns(uint256) {
+    function balanceOf(address _owner)  public override view returns(uint256) {
         require(_owner != address(0), "Error, query on nonexistent address");
         return _ownedTokens[_owner];
     }
 
     // owner of function returns the owner of an NFT token id
-    function ownerOf(uint256 _tokenId) public view returns (address) {
+    function ownerOf(uint256 _tokenId) public override view returns (address) {
         require(_exists(_tokenId), "Error, owner query for nonexistent token");
         address owner = _tokenOwner[_tokenId];
         return owner;
@@ -80,7 +72,7 @@ contract ERC721 is ERC165 {
         emit Transfer(_from, _to, _tokenId);
     }
 
-    function transferFrom(address _from, address _to, uint256 _tokenId) public {
+    function transferFrom(address _from, address _to, uint256 _tokenId) public override {
         require(_isApprovedOrOwner(_to, _tokenId), "Error, spender is not approved or owner");
         _transferFrom(_from, _to, _tokenId);
     }
@@ -89,25 +81,25 @@ contract ERC721 is ERC165 {
     // approve an address to a tokenId (token)
     // require that we can't send tokens of the owner to the owner (current caller)
     // update mapping of _tokenApprovals
-    function approve(address _to, uint256 tokenId) public {
+    /* function approve(address _to, uint256 tokenId) public {
         address owner = ownerOf(tokenId);
         require(_to != owner, "Error, token transfer approval to current owner!");
         require(msg.sender == owner, "Error, current caller is not the owner of the token!");
         _tokenApprovals[tokenId] = _to;
         emit Approval(owner, _to, tokenId);
-    }
+    } */
 
     function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns(bool) {
         require(_exists(tokenId), "Error, token does not exist");
         address owner = ownerOf(tokenId);
-        return(spender == owner || getApproved(tokenId) == spender);
+        return(spender == owner /* || getApproved(tokenId) == spender */);
     }
     
     // returns the approved send to address for the tokenId
-    function getApproved(uint256 tokenId) public view returns (address) {
+    /* function getApproved(uint256 tokenId) public view returns (address) {
         require(_exists(tokenId), "ERC721: approved query for nonexistent token");
 
         return _tokenApprovals[tokenId];
-    }
+    } */
     
 }
