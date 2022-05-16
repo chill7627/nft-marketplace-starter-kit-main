6360a13a7aa50553542be3643f731e6f14bd6494
chill7627 <sethhill7627@gmail.com>
2022-05-15 21:59:55 -0400

    fixing metamask not connecting bug

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     async componentDidMount() {
         await this.loadWeb3();
+        await this.loadBlockChainData();
     }
 
     // first detect ethereum provider
@@ -19,16 +20,42 @@ class App extends Component {
         if(provider) {
             console.log('ethereum wallet is connected');
             window.web3 = new Web3(provider);
+            let web3 = window.web3;
         } else {
             // no ethereum provider
-            console.log('no ethereum wallet detected.')
+            console.log('no ethereum wallet detected.');
         }
+    }
+
+    async loadBlockChainData() {
+        const web3 = window.web3;
+        const accounts = await web3.eth.getAccounts();
+        this.setState({account:accounts});
+        console.log(this.state.account);
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            account: ''
+        };
     }
 
     render() {
         return (
             <div>
-                <h1>NFT Marketplace</h1>
+                <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
+                    <div className='navbar-brand col-sm-3 col-md-3 mr-0' style={{color:'white'}}>
+                        Krypto Birdz NFTS (Non Fungible Tokens)
+                    </div>
+                    <ul className='navbar-nav px-3'>
+                        <li className='nav-item text-nowwrap d-none d-sm-none d-sm-block'>
+                            <small className="text-white">
+                                {this.state.account}
+                            </small>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         )
     }
