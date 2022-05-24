686660b88c00de1e64cfd2795c003bd265ccbed8
chill7627 <sethhill7627@gmail.com>
2022-05-23 21:50:33 -0400

    added in minting to front end

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,6 @@ class App extends Component {
         if(provider) {
             console.log('ethereum wallet is connected');
             window.web3 = new Web3(provider);
-            let web3 = window.web3;
         } else {
             // no ethereum provider
             console.log('no ethereum wallet detected.');
@@ -30,20 +29,58 @@ class App extends Component {
     async loadBlockChainData() {
         const web3 = window.web3;
         const accounts = await web3.eth.getAccounts();
-        this.setState({account:accounts});
-        console.log(this.state.account);
+        this.setState({account:accounts[0]});
+
+        const networkId = await web3.eth.net.getId();
+        const networkData = KryptoBird.networks[networkId];
+        if(networkData) {
+            const abi = KryptoBird.abi;
+            const address = networkData.address;
+            const contract = new web3.eth.Contract(abi, address);
+            this.setState({contract});
+
+            // call total supply of crypto birds
+            const totalSupply = await contract.methods.totalSupply().call();
+            this.setState({totalSupply});
+
+            // keep track of tokens in an array
+            for(let i = 1; i <= totalSupply; i++) {
+                const KryptoBird = await contract.methods.kryptoBirdz(i - 1).call();
+                this.setState({
+                    kryptoBirdz:[...this.state.kryptoBirdz, KryptoBird]
+                })
+                
+            }
+        } else {
+            window.alert('Smart contract not deployed.');
+        }
+    }
+
+    // minting, want to send information, we need to specify the account 
+    mint = (kryptoBird) => {
+        this.state.contract.methods.mint(kryptoBird).send({from:this.state.account})
+        .once('receipt', (receipt)=> {
+            this.setState({
+                kryptoBirdz:[...this.state.kryptoBirdz, kryptoBird]
+            })
+        })
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            account: ''
+            account: '',
+            contract: null,
+            totalSupply: 0,
+            kryptoBirdz:[]
         };
     }
 
     render() {
         return (
             <div>
+                {console.log(this.state.kryptoBirdz)}
+                {console.log(this.state.totalSupply)}
                 <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
                     <div className='navbar-brand col-sm-3 col-md-3 mr-0' style={{color:'white'}}>
                         Krypto Birdz NFTS (Non Fungible Tokens)
@@ -56,6 +93,35 @@ class App extends Component {
                         </li>
                     </ul>
                 </nav>
+
+                <div className='container-fluid mt-1'>
+                    <div className='row'>
+                        <main role='main' className='col-lg d-flex text-center'>
+                            <div className="content mr-auto ml-auto" style={{opacity:'0.8'}}>
+                                <h1 style={{color:'white'}}>KryptoBirdz NFT Marketplace</h1>
+
+                                <form onSubmit={(event)=>{
+                                    event.preventDefault();
+                                    const kryptoBird = this.kryptoBird.value;
+                                    this.mint(kryptoBird);
+                                }}>
+                                    <input type='text' placeholder="Add a file location" className='mb-1'
+                                    ref={(input)=>this.kryptoBird = input}>
+
+                                    </input>
+
+                                    <input type='submit' className='btn btn-primary btn-black' value='MINT' sytle={{margin:'6px'}}>
+
+                                    </input>
+
+                                </form>
+                            </div>
+                        </main>
+
+                    </div>
+                </div>
+
+
             </div>
         )
     }
